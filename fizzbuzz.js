b5863017401d3dce5c1fb5d4735680a9e8f79b08
sandesydney <sandesydney01@gmail.com>
2022-10-13 14:35:49 +0300

    FizzBuzz upload

---
+++ fizzbuzz.js
@@ -0,0 +1,23 @@
+// variable to hold current number value
+let num = 1
+
+// Function to print Fizz and Buzz and increase num to 100
+function fizzBuzz(){
+    if(num > 100){
+        return
+    }
+    if(num % 3 == 0){
+        console.log("Fizz")
+    } else if(num % 5 == 0){
+        console.log("Buzz",num)
+    } else if(num % 3 == 0 && num % 5 == 0){
+        console.log("FizzBuzz")
+    }else{
+        console.log(num)
+    }
+    num++
+    fizzBuzz()
+}
+
+// Function call
+fizzBuzz()
